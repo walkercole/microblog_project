463d9b29c7d0d8f9ee56ed7eab324c62d53efacd
Walker Cole <besidethebrave@gmail.com>
2016-12-15 14:49:00 -0500

    fixes #1 Added sign-in, sign-out, and sign-up functionality

--- app.rb
+++ app.rb
@@ -1,9 +1,41 @@
 require "sinatra"
+require "sinatra/flash"
 require "sinatra/activerecord"
 
 set :database, {adapter: "sqlite3", database: "mydb.sqlite3"}
 require "./models"
 
+enable :sessions
+
+def current_user
+	if session[:user_id]
+		User.find session[:user_id]
+	end
+end
+
 get "/" do
-	"hello world"
+	erb :index
+end
+
+post "/sign-up" do 
+	@user = User.create(username: params["regname"], password: params["regpass"])
+	flash[:notice] = "User Created! Please Login."
+	redirect "/"
+end
+
+post "/login" do 
+	@user = User.find_by username: params["username"]
+	if @user && @user.password == params["password"]
+		session[:user_id] = @user.id
+		flash[:notice] = "Welcome Back!"
+	else 
+		flash[:error] = "Login Failed"
+	end
+	redirect "/"
+end
+
+get "/logout" do
+	session[:user_id] = nil
+	flash[:notice] = "Later &lt;/3"
+	redirect "/"
 end
